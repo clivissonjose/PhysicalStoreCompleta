978afc17d216ca1331938a0e43f023557a40d2cd
clivissonjose <cjbarbosa2001@gmail.com>
2025-01-10 22:33:35 -0300

    refactoring store by cep

--- src/stores/store.service.spec.ts
+++ src/stores/store.service.spec.ts
@@ -1,12 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StoreService } from './store.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { CalculateFrete } from './services/calculate-frete';
+import { CalculateDistances } from './services/calculate-distances';
+import { CalculateCoordinates } from 'src/stores/services/calculate-coordenates';
 
 describe('StoreService', () => {
   let service: StoreService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StoreService],
+      providers: [StoreService,{
+        provide: getModelToken('Store'), // Mock do Model do Mongoose
+        useValue: {
+          find: jest.fn(),
+          findById: jest.fn(),
+          save: jest.fn(),
+        },
+      },
+      {
+        provide: CalculateCoordinates,
+        useValue: {
+          searchCoordinates: jest.fn(),
+        },
+      },
+      {
+        provide: CalculateFrete,
+        useValue: {
+          calcularFrete: jest.fn(),
+        },
+      },
+      {
+        provide: CalculateDistances,
+        useValue: {
+          calculateDistances: jest.fn(),
+        },
+      }],
+      
     }).compile();
 
     service = module.get<StoreService>(StoreService);
